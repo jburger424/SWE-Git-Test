59a17818c4bc4678c1f155c14a870d45a5092b89
Jon Burger <jburger424@gmail.com>
2018-01-29 10:53:10 -0500

    resolved failing 1 number tests

--- src/com/company/Adder.java
+++ src/com/company/Adder.java
@@ -3,6 +3,9 @@ package com.company;
 public class Adder {
   public int add(String numbers){
     if(numbers.length() == 0) return 0;
+    if(numbers.indexOf(' ') == -1){
+      return Integer.parseInt(numbers);
+    }
     return -1;
   }
 }
