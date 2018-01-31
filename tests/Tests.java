08b4d2a6b6bc209af4eb11976deb8edd9c740b71
Jon Burger <jburger424@gmail.com>
2018-01-31 07:07:05 -0500

    tests written and passing for unknown number of arguments

--- tests/Tests.java
+++ tests/Tests.java
@@ -11,5 +11,7 @@ public class Tests {
     assertEquals(101,adder.add("101"),"'101' does NOT return 101");
     assertEquals(4,adder.add("1,3"),"'1,3' does NOT return 4");
     assertEquals(2,adder.add("-1,3"),"'-1,3' does NOT return 2");
+    assertEquals(6,adder.add("1,2,3"),"'1,2,3' does NOT return 6");
+    assertEquals(77,adder.add("3,1,4,1,5,9,2,6,5,3,5,8,9,7,9"),"'3,1,4,1,5,9,2,6,5,3,5,8,9,7,9' does NOT return 77");
   }
 }
