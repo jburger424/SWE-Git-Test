be6033ac56edd5462368cbd532d935ba23aff854
O'Grady <nymlogrady@gmail.com>
2018-02-01 10:26:36 -0500

    fixed ',#' problem

--- src/com/company/Adder.java
+++ src/com/company/Adder.java
@@ -2,12 +2,13 @@ package com.company;
 
 public class Adder {
   public int add(String numbers){
-    if(numbers.length() == 0) return 0;
     int sum = 0;
     String[] splitNums = numbers.split(",");
     for(String numStr : splitNums){
-      int num = Integer.parseInt(numStr);
-      sum += num;
+      if(numStr.length()>0) {
+        int num = Integer.parseInt(numStr);
+        sum += num;
+      }
     }
     return sum;
   }
